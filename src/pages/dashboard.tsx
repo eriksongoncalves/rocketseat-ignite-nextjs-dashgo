48639bf05f7e68edddbda24b3b8068610bfbf622
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-04-03 15:16:40 -0300

    feat: sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,14 +1,21 @@
 import Head from 'next/head';
+import { Flex } from '@chakra-ui/react';
 
-import { Header } from '../components/Header';
+import { Header, Sidebar } from '../components';
 
 function Dashboard() {
   return (
     <>
       <Head>
-        <title>DashGo</title>
+        <title>Dashboard | DashGo</title>
       </Head>
-      <Header />
+
+      <Flex direction="column" h="100vh">
+        <Header />
+        <Flex w="100%" my="6" maxWidth="1480" mx="auto" px="6">
+          <Sidebar />
+        </Flex>
+      </Flex>
     </>
   );
 }
